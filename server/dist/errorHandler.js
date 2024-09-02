d9f7c985f5d39d460e3d3a54f6860390bd0f67a8
Vaibhav <vs630916@gmail.com>
2024-09-03 00:37:04 +0530

    Deploymental changes1.2

--- server/dist/errorHandler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorHandler = void 0;
-const errorHandler = (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ message: err.message });
-};
-exports.errorHandler = errorHandler;
--- server/dist/routes/artistRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const artistConroller_1 = require("../controllers/artistConroller");
-const router = (0, express_1.Router)();
-router.get("/search", artistConroller_1.getArtists);
-exports.default = router;
