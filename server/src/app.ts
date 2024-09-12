c9a5a5cc432b4156e27d468b8522228b16575262
Vaibhav <vs630916@gmail.com>
2024-09-12 22:50:06 +0530

    Deploymental Changes 2.6

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,7 +10,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+}));
 app.use(express.json());
 app.use(errorHandler);
 app.use("/api/artists", artistRouter);
